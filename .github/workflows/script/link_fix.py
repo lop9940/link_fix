df107d51a78c8d0a4840b78e09e236dbde544ea6
lop9940 <qaqclop9940@gmail.com>
2022-08-16 19:31:11 +0900

    path fix

--- .github/workflows/script/link_fix.py
+++ .github/workflows/script/link_fix.py
@@ -77,15 +77,15 @@ def re_pattern():
 
 
 
-header,mermeid,footer=file_lines_split("../../../P01_test.md","```mermaid","```")
+header,mermeid,footer=file_lines_split("P01_test.md","```mermaid","```")
 
 new_mermaid = generate_mermaid_lines(mermeid) 
 
 new_lines=header+new_mermaid+footer
 
-shutil.rmtree("../../../backup")
-os.mkdir("../../../backup")
-shutil.copy("../../../P01_test.md","../../../backup/P01_test_backup.md")
+shutil.rmtree("backup")
+os.mkdir("backup")
+shutil.copy("P01_test.md","backup/P01_test_backup.md")
 
-with open("../../../P01_test.md", "w") as file:
+with open("P01_test.md", "w") as file:
   file.write("\n".join(new_lines))
