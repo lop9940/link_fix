75a5ef4241c24a2be9f370683c3ca5943716304d
lop9940 <qaqclop9940@gmail.com>
2022-08-19 21:35:38 +0900

    add get process folder information

--- .github/workflows/add_meraid_link/main.py
+++ .github/workflows/add_meraid_link/main.py
@@ -1,3 +1,5 @@
+import os
+import shutil
 import pathlib
 import name
 from split_file import split_file
@@ -7,21 +9,25 @@ from update_file import update_file
 
 def main():
 
-    process_dir_Path = pathlib.Path("process")
+    process_dir_Path = pathlib.Path(name.P_dir)
     target_files_Path = process_dir_Path.iterdir()
-    backup_dir_path = str(pathlib.Path("backup"))
+    backup_dir_path = str(pathlib.Path(name.backup_dir))
+    shutil.rmtree(backup_dir_path)
+    os.mkdir(backup_dir_path)
 
     for file_Path in target_files_Path:
         if not file_Path.is_file():
             continue
 
-        header, mermeid, footer = split_file(str(file_Path))
+        file_path = str(file_Path)
+
+        header, mermeid, footer = split_file(file_path)
 
         new_lines = update_markdown(header, mermeid, footer)
 
         print(new_lines)
 
-        # update_file(file_path, backup_path, new_lines)
+        update_file(new_lines, file_path, backup_dir_path)
 
 
 if __name__ == "__main__":
